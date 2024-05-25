95981b97f540d6b12c5943b3c74e2a4218b95233
mis <satoutahhaithem@gmail.com>
2024-05-25 16:55:34 +0200

    add result of ceplex pc mis

--- solveWithCeplex.sh
+++ solveWithCeplex.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Define the timeout duration (if needed)
-timeout_duration="10s"
+timeout_duration="3600s"
 
 # Base directories Pc Heythem
-base_output_dir="./outputsCeplexPcHeythem"
-base_time_dir="./TimeSolvingCeplexPcHeythem"
+# base_output_dir="./outputsCeplexPcHeythem"
+# base_time_dir="./TimeSolvingCeplexPcHeythem"
 # Base directories Pc Mis
-# base_output_dir="./outputsCeplex"
-# base_time_dir="./TimeSolvingCeplex"
+base_output_dir="./outputsCeplex"
+base_time_dir="./TimeSolvingCeplex"
 
 
 # Iterate over the years
@@ -16,10 +16,10 @@ for yearRodef in {2024..2021}; do
     output_dir="${base_output_dir}/${yearRodef}"
     time_dir="${base_time_dir}/${yearRodef}"
     
-    max_parallel_sessions_range_2024=($(seq 15 -1 9))
-    max_parallel_sessions_range_2023=($(seq 18 -1 11))
-    max_parallel_sessions_range_2022=($(seq 16 -1 10))
-    max_parallel_sessions_range_2021=($(seq 10 -1 4))
+    max_parallel_sessions_range_2024=($(seq 15 -1 10))
+    max_parallel_sessions_range_2023=($(seq 18 -1 12))
+    max_parallel_sessions_range_2022=($(seq 16 -1 11))
+    max_parallel_sessions_range_2021=($(seq 10 -1 5))
 
     case $yearRodef in
         2024)
