aa5571f3a76b95dd9a5e1724d657aefd24f518f4
mis <satoutahhaithem@gmail.com>
2024-05-26 22:27:12 +0200

    prepare script for timeout

--- solveWithCeplex.sh
+++ solveWithCeplex.sh
@@ -17,47 +17,47 @@ base_output_dir="./PcMis/outputsCeplex"
 base_time_dir="./PcMis/TimeSolvingCeplex"
 
 
-# Iterate over the years
-for yearRodef in {2024..2021}; do
-    output_dir="${base_output_dir}/${yearRodef}"
-    time_dir="${base_time_dir}/${yearRodef}"
+# # Iterate over the years
+# for yearRodef in {2024..2021}; do
+#     output_dir="${base_output_dir}/${yearRodef}"
+#     time_dir="${base_time_dir}/${yearRodef}"
     
-    max_parallel_sessions_range_2024=($(seq 15 -1 10))
-    max_parallel_sessions_range_2023=($(seq 18 -1 12))
-    max_parallel_sessions_range_2022=($(seq 16 -1 11))
-    max_parallel_sessions_range_2021=($(seq 10 -1 5))
+#     max_parallel_sessions_range_2024=($(seq 15 -1 10))
+#     max_parallel_sessions_range_2023=($(seq 18 -1 12))
+#     max_parallel_sessions_range_2022=($(seq 16 -1 11))
+#     max_parallel_sessions_range_2021=($(seq 10 -1 5))
 
-    case $yearRodef in
-        2024)
-            max_parallel_sessions_range=("${max_parallel_sessions_range_2024[@]}")
-            ;;
-        2023)
-            max_parallel_sessions_range=("${max_parallel_sessions_range_2023[@]}")
-            ;;
-        2022)
-            max_parallel_sessions_range=("${max_parallel_sessions_range_2022[@]}")
-            ;;
-        2021)
-            max_parallel_sessions_range=("${max_parallel_sessions_range_2021[@]}")
-            ;;
-        *)
-            echo "Year not supported"
-            exit 1
-    esac
+#     case $yearRodef in
+#         2024)
+#             max_parallel_sessions_range=("${max_parallel_sessions_range_2024[@]}")
+#             ;;
+#         2023)
+#             max_parallel_sessions_range=("${max_parallel_sessions_range_2023[@]}")
+#             ;;
+#         2022)
+#             max_parallel_sessions_range=("${max_parallel_sessions_range_2022[@]}")
+#             ;;
+#         2021)
+#             max_parallel_sessions_range=("${max_parallel_sessions_range_2021[@]}")
+#             ;;
+#         *)
+#             echo "Year not supported"
+#             exit 1
+#     esac
 
 
-    # All the instances of one year
-    for max_parallel_sessions in "${max_parallel_sessions_range[@]}"; do
-        output_file="${output_dir}/${max_parallel_sessions}_session_output.txt"
-        time_file="${time_dir}/${max_parallel_sessions}_session_time.txt"
+#     # All the instances of one year
+#     for max_parallel_sessions in "${max_parallel_sessions_range[@]}"; do
+#         output_file="${output_dir}/${max_parallel_sessions}_session_output.txt"
+#         time_file="${time_dir}/${max_parallel_sessions}_session_time.txt"
 
-        {
-            time timeout --signal=INT "$timeout_duration" python3 ./main.py "$yearRodef" "$max_parallel_sessions" > "$output_file"
-        } 2> "$time_file"
-    done
+#         {
+#             time timeout --signal=INT "$timeout_duration" python3 ./main.py "$yearRodef" "$max_parallel_sessions" > "$output_file"
+#         } 2> "$time_file"
+#     done
 
-    echo "Execution times and outputs for year $yearRodef recorded in respective files."
-done
+#     echo "Execution times and outputs for year $yearRodef recorded in respective files."
+# done
 
 # Define the years and corresponding max parallel sessions
 years=("2024" "2023" "2022" "2021")
@@ -102,51 +102,51 @@ base_output_dir_without_z="./PcMis/WithoutZoutputsCeplex"
 base_time_dir_without_z="./PcMis/WithoutZTimeSolvingCeplex"
 
 
-for yearRodef in {2024..2021}; do
-    output_dir="${base_output_dir_without_z}/${yearRodef}"
-    time_dir="${base_time_dir_without_z}/${yearRodef}"
+# for yearRodef in {2024..2021}; do
+#     output_dir="${base_output_dir_without_z}/${yearRodef}"
+#     time_dir="${base_time_dir_without_z}/${yearRodef}"
 
-    # Ensure directories exist
-    mkdir -p "$output_dir"
-    mkdir -p "$time_dir"
+#     # Ensure directories exist
+#     mkdir -p "$output_dir"
+#     mkdir -p "$time_dir"
     
-    # Define max parallel sessions range for each year
-    max_parallel_sessions_range_2024=($(seq 15 -1 10))
-    max_parallel_sessions_range_2023=($(seq 18 -1 12))
-    max_parallel_sessions_range_2022=($(seq 16 -1 11))
-    max_parallel_sessions_range_2021=($(seq 10 -1 5))
+#     # Define max parallel sessions range for each year
+#     max_parallel_sessions_range_2024=($(seq 15 -1 10))
+#     max_parallel_sessions_range_2023=($(seq 18 -1 12))
+#     max_parallel_sessions_range_2022=($(seq 16 -1 11))
+#     max_parallel_sessions_range_2021=($(seq 10 -1 5))
 
-    # Select the appropriate range for the current year
-    case $yearRodef in
-        2024)
-            max_parallel_sessions_range=("${max_parallel_sessions_range_2024[@]}")
-            ;;
-        2023)
-            max_parallel_sessions_range=("${max_parallel_sessions_range_2023[@]}")
-            ;;
-        2022)
-            max_parallel_sessions_range=("${max_parallel_sessions_range_2022[@]}")
-            ;;
-        2021)
-            max_parallel_sessions_range=("${max_parallel_sessions_range_2021[@]}")
-            ;;
-        *)
-            echo "Year not supported"
-            exit 1
-    esac
+#     # Select the appropriate range for the current year
+#     case $yearRodef in
+#         2024)
+#             max_parallel_sessions_range=("${max_parallel_sessions_range_2024[@]}")
+#             ;;
+#         2023)
+#             max_parallel_sessions_range=("${max_parallel_sessions_range_2023[@]}")
+#             ;;
+#         2022)
+#             max_parallel_sessions_range=("${max_parallel_sessions_range_2022[@]}")
+#             ;;
+#         2021)
+#             max_parallel_sessions_range=("${max_parallel_sessions_range_2021[@]}")
+#             ;;
+#         *)
+#             echo "Year not supported"
+#             exit 1
+#     esac
 
-    # Loop through max parallel sessions
-    for max_parallel_sessions in "${max_parallel_sessions_range[@]}"; do
-        output_file="${output_dir}/${max_parallel_sessions}_session_output.txt"
-        time_file="${time_dir}/${max_parallel_sessions}_session_time.txt"
+#     # Loop through max parallel sessions
+#     for max_parallel_sessions in "${max_parallel_sessions_range[@]}"; do
+#         output_file="${output_dir}/${max_parallel_sessions}_session_output.txt"
+#         time_file="${time_dir}/${max_parallel_sessions}_session_time.txt"
 
-        {
-            time timeout --signal=INT "$timeout_duration" python3 ./main.py "$yearRodef" "$max_parallel_sessions" 0 > "$output_file"
-        } 2> "$time_file"
-    done
+#         {
+#             time timeout --signal=INT "$timeout_duration" python3 ./main.py "$yearRodef" "$max_parallel_sessions" 0 > "$output_file"
+#         } 2> "$time_file"
+#     done
 
-    echo "Execution times and outputs for year $yearRodef recorded in respective files."
-done
+#     echo "Execution times and outputs for year $yearRodef recorded in respective files."
+# done
 
 # Define the years and corresponding max parallel sessions
 years=("2024" "2023" "2022" "2021")
