e352b9816c4e972d554a7eaa6bf414f55504c9e4
satoutahhaithem <satoutahhaithem>
2024-05-26 14:55:00 +0200

    convert variable to binary variables

--- main.py
+++ main.py
@@ -1,116 +1,50 @@
 from docplex.mp.model import Model
 import sys
 import docplex.mp.conflict_refiner as cr
-# Define sets
-# conference_sessions = 47
-# slots = 7
-# papers_range = [3,4,5,6]
-# max_parallel_sessions =14
-# NbrGrps = 24 
-# np= [6,3,7,8,8,4,12,4,15,10,11,12,14,12,8,10,8,
-#         5,3,12,3,11,11,3,11,3,6,3,3,3,10,4,6,4,4,5,3,7,3,6,7,12,8,17,6,10,17,2]
-# npMax = [5,4,4,6,5,4,4]
-
-# # Define the working groups associated with each session
-# session_groups = [  
-#     [17],[7],[13],[5],[4],[],[18,6,22],[18,6],[8],[9],[5],[12],[],[7],[16],[23],[13],[4],[],[],[9,5],
-#     [2,18],[],[11],[3],[18],[18,6],[5,8],[16,3],[19],[14],[5],[20,15,21],[10],[10],[13],[2,5],[],[],
-#     [5],[8,9],[7],[13],[5,4],[18,6],[9],[1,24]
-# ]
-# length_of_paper_range = len(papers_range)
-# Sessions = range(1, conference_sessions + 1)
-# Slots = range(1, slots + 1)
-# PaperRangeIndex = range(1, length_of_paper_range + 1)
-# Groups = range(1, NbrGrps + 1)
-
-
-# conference_sessions = 40
-# slots = 7
-# length_of_paper_range = 4
-# NbrGrps = 20
-
-# Sessions = range(1, conference_sessions + 1)
-# Slots = range(1, slots + 1)
-# PaperRangeIndex = range(1, length_of_paper_range + 1)
-# Groups = range(1, NbrGrps + 1)
-
-# # Parameters
-# papers_range = [3, 4, 5, 6]
-# np = [14, 23, 12, 9, 9, 6, 10, 4, 10, 7, 6, 5, 3, 5, 6, 4, 3, 12, 7, 16, 4, 5, 14, 11, 4, 3, 10, 6, 6, 4, 13, 3, 4, 9, 5, 4, 11, 6, 6, 8]
-# npMax = [4, 6, 6, 4, 4, 5, 3]
-# max_parallel_sessions = 11
-# session_groups = [
-#     [1], [2], [3], [], [], [], [6], [7], [7, 8], [10], [8], [8, 11], [5, 8], 
-#     [3, 8], [7], [13], [13], [14], [], [8], [16], [16], [20], [17], [13], 
-#     [], [9], [11], [11, 12], [9], [6, 19], [], [], [18], [10], [5], [16], 
-#     [4, 5], [8, 12], [7, 15]
-# ]
-
-# Define sets
-# conference_sessions = 3
-# slots = 2
-# length_of_paper_range = 2
-# NbrGrps = 2
-
-# Sessions = range(1, conference_sessions + 1)
-# Slots = range(1, slots + 1)
-# PaperRangeIndex = range(1, length_of_paper_range + 1)
-# Groups = range(1, NbrGrps + 1)
-
-# # Parameters
-# papers_range = [2, 3]
-# np = [3, 3, 3]
-# npMax = [2, 3]
-# max_parallel_sessions = 2
-# session_groups = [[1], [2], [3]]
 
 if len(sys.argv) < 1:
     print("You must specify the year and maxparrallel session as a parameter (e.g., python3 Scheduling_Problem.py 2024 11)")
     sys.exit(1)
 
-# recover the first argument
+# Recover the first argument
 data_set_choice = sys.argv[1]
 
-# recover the second parameter 
+# Recover the second parameter 
 max_parallel_sessions =  int(sys.argv[2])
 
 if data_set_choice == "2024":
-    if (max_parallel_sessions < 0 ): 
-        print ("max_parallel_sessions must more than 9")
+    if (max_parallel_sessions < 0): 
+        print("max_parallel_sessions must more than 9")
         sys.exit(1) 
     else:
         from Data.ROADEF2024 import conference_sessions, slots, papers_range, working_groups, np, npMax, session_groups
 elif data_set_choice == "2023":
-    if (max_parallel_sessions < 0 ): 
-        print ("max_parallel_sessions must be more than 12")
+    if (max_parallel_sessions < 0): 
+        print("max_parallel_sessions must be more than 12")
         sys.exit(1) 
     else:
         from Data.ROADEF2023 import conference_sessions, slots, papers_range, working_groups, np, npMax, session_groups 
 elif data_set_choice == "2022":
-    if (max_parallel_sessions < 0 ): 
-        print ("max_parallel_sessions must be more than 11")
+    if (max_parallel_sessions < 0): 
+        print("max_parallel_sessions must be more than 11")
         sys.exit(1) 
     else:
         from Data.ROADEF2022 import conference_sessions, slots, papers_range, working_groups, np, npMax, session_groups 
-elif  data_set_choice == "2021":
-    if (max_parallel_sessions < 0 ): 
-        print ("max_parallel_sessions must be more than 5")
+elif data_set_choice == "2021":
+    if (max_parallel_sessions < 0): 
+        print("max_parallel_sessions must be more than 5")
         sys.exit(1) 
     else:
         from Data.ROADEF2021 import conference_sessions, slots, papers_range, working_groups, np, npMax, session_groups  
 else:
-    print("The data available for 2024 , 2023 , 2022 and 2021 only")
+    print("The data available for 2024, 2023, 2022, and 2021 only")
     sys.exit(1)
-
-
 
 length_of_paper_range = len(papers_range)
 Sessions = range(1, conference_sessions + 1)
 Slots = range(1, slots + 1)
 PaperRangeIndex = range(1, length_of_paper_range + 1)
 Groups = range(1, working_groups + 1)
-
-
 
 def var_x(s, c, l):
     return (s-1) * slots * length_of_paper_range + (c-1) * length_of_paper_range + l
@@ -124,8 +58,8 @@ def var_z(s, c):
 mdl = Model()
 
 # Decision Variables
-x = {(s, c, l): mdl.integer_var(name=f'x_{s}_{c}_{l}') for s in Sessions for c in Slots for l in PaperRangeIndex}
-y = mdl.integer_var_dict(((s1, s2, c, g) for s1 in Sessions for s2 in Sessions if s1 < s2 for c in Slots for g in Groups), name="y")
+x = {(s, c, l): mdl.binary_var(name=f'x_{s}_{c}_{l}') for s in Sessions for c in Slots for l in PaperRangeIndex}
+y = mdl.binary_var_dict(((s1, s2, c, g) for s1 in Sessions for s2 in Sessions if s1 < s2 for c in Slots for g in Groups), name="y")
 z = {(s, c): mdl.binary_var(name=f'z_{s}_{c}') for s in Sessions for c in Slots}
 session_allocated = mdl.binary_var_matrix(Sessions, Slots, name="session_allocated")
 
@@ -151,7 +85,8 @@ for s in Sessions:
 
 # Fourth Constraint: Number of parallel sessions is not exceeded for each slot
 for c in Slots:
-        mdl.add_constraint(mdl.sum(z[s, c] for s in Sessions) <= max_parallel_sessions)
+    mdl.add_constraint(mdl.sum(z[s, c] for s in Sessions) <= max_parallel_sessions)
+
 # Implementing equivalence transformation for z variables
 for s in Sessions:
     for c in Slots:
@@ -186,13 +121,11 @@ def display_assignments_by_slot_with_counts(model, slots, papers_range, conferen
             for l in PaperRangeIndex:
                 if x[s, c, l].solution_value > 0:  # Check if the variable is assigned
                     print(f"  Conference Session {s} in slot {c} with {papers_range[l-1]} papers")
-print ("max_parallel_sessions = " , max_parallel_sessions)
+
+print("max_parallel_sessions =", max_parallel_sessions)
 if solution:
-    print (solution)
+    print(solution)
     # The problem is feasible, display the solution
-    # display_assignments_by_slot_with_counts(solution, slots, papers_range, conference_sessions)
-    # Print the number of conflicts
-    # print(solution)
     total_conflicts = num_conflicts.solution_value
     print(f"Total number of conflicts: {total_conflicts}")
 else:
@@ -202,4 +135,3 @@ else:
         cref = cr.ConflictRefiner()
         print('Showing some of the constraints that can be removed to arrive at a minimal conflict:')
         cref.refine_conflict(mdl, display=True)
-
--- solveWithCeplex.sh
+++ solveWithCeplex.sh
@@ -4,75 +4,75 @@
 timeout_duration="3600s"
 
 # Base directories Pc Heythem
-# base_output_dir="./outputsCeplexPcHeythem"
-# base_time_dir="./TimeSolvingCeplexPcHeythem"
+base_output_dir="./outputsCeplexPcHeythem"
+base_time_dir="./TimeSolvingCeplexPcHeythem"
 # Base directories Pc Mis
-base_output_dir="./outputsCeplex"
-base_time_dir="./TimeSolvingCeplex"
+# base_output_dir="./outputsCeplex"
+# base_time_dir="./TimeSolvingCeplex"
 
 
 # Iterate over the years
-# for yearRodef in {2024..2021}; do
-#     output_dir="${base_output_dir}/${yearRodef}"
-#     time_dir="${base_time_dir}/${yearRodef}"
-    
-#     max_parallel_sessions_range_2024=($(seq 15 -1 10))
-#     max_parallel_sessions_range_2023=($(seq 18 -1 12))
-#     max_parallel_sessions_range_2022=($(seq 16 -1 11))
-#     max_parallel_sessions_range_2021=($(seq 10 -1 5))
-
-#     case $yearRodef in
-#         2024)
-#             max_parallel_sessions_range=("${max_parallel_sessions_range_2024[@]}")
-#             ;;
-#         2023)
-#             max_parallel_sessions_range=("${max_parallel_sessions_range_2023[@]}")
-#             ;;
-#         2022)
-#             max_parallel_sessions_range=("${max_parallel_sessions_range_2022[@]}")
-#             ;;
-#         2021)
-#             max_parallel_sessions_range=("${max_parallel_sessions_range_2021[@]}")
-#             ;;
-#         *)
-#             echo "Year not supported"
-#             exit 1
-#     esac
-
-
-#     # All the instances of one year
-#     for max_parallel_sessions in "${max_parallel_sessions_range[@]}"; do
-#         output_file="${output_dir}/${max_parallel_sessions}_session_output.txt"
-#         time_file="${time_dir}/${max_parallel_sessions}_session_time.txt"
-
-#         {
-#             time timeout --signal=INT "$timeout_duration" python3 ./main.py "$yearRodef" "$max_parallel_sessions" > "$output_file"
-#         } 2> "$time_file"
-#     done
-
-#     echo "Execution times and outputs for year $yearRodef recorded in respective files."
-# done
-
-# Define the years and corresponding max parallel sessions
-years=("2024" "2023" "2022" "2021")
-sessions=("9" "11" "10" "4")
-
-# Loop through each year and session
-for i in ${!years[@]}; do
-    yearRodef=${years[$i]}
-    max_parallel_sessions=${sessions[$i]}
-    
+for yearRodef in {2024..2021}; do
     output_dir="${base_output_dir}/${yearRodef}"
     time_dir="${base_time_dir}/${yearRodef}"
     
+    max_parallel_sessions_range_2024=($(seq 15 -1 10))
+    max_parallel_sessions_range_2023=($(seq 18 -1 12))
+    max_parallel_sessions_range_2022=($(seq 16 -1 11))
+    max_parallel_sessions_range_2021=($(seq 10 -1 5))
+
+    case $yearRodef in
+        2024)
+            max_parallel_sessions_range=("${max_parallel_sessions_range_2024[@]}")
+            ;;
+        2023)
+            max_parallel_sessions_range=("${max_parallel_sessions_range_2023[@]}")
+            ;;
+        2022)
+            max_parallel_sessions_range=("${max_parallel_sessions_range_2022[@]}")
+            ;;
+        2021)
+            max_parallel_sessions_range=("${max_parallel_sessions_range_2021[@]}")
+            ;;
+        *)
+            echo "Year not supported"
+            exit 1
+    esac
 
 
-    output_file="${output_dir}/${max_parallel_sessions}_session_output.txt"
-    time_file="${time_dir}/${max_parallel_sessions}_session_time.txt"
+    # All the instances of one year
+    for max_parallel_sessions in "${max_parallel_sessions_range[@]}"; do
+        output_file="${output_dir}/${max_parallel_sessions}_session_output.txt"
+        time_file="${time_dir}/${max_parallel_sessions}_session_time.txt"
 
-    {
-        time timeout --signal=INT "$timeout_duration" python3 ./main.py "$yearRodef" "$max_parallel_sessions" > "$output_file"
-    } 2> "$time_file"
+        {
+            time timeout --signal=INT "$timeout_duration" python3 ./main.py "$yearRodef" "$max_parallel_sessions" > "$output_file"
+        } 2> "$time_file"
+    done
+
+    echo "Execution times and outputs for year $yearRodef recorded in respective files."
 done
 
-echo "Execution times and outputs for all specified years recorded in respective files."
+# Define the years and corresponding max parallel sessions
+# years=("2024" "2023" "2022" "2021")
+# sessions=("9" "11" "10" "4")
+
+# # Loop through each year and session
+# for i in ${!years[@]}; do
+#     yearRodef=${years[$i]}
+#     max_parallel_sessions=${sessions[$i]}
+    
+#     output_dir="${base_output_dir}/${yearRodef}"
+#     time_dir="${base_time_dir}/${yearRodef}"
+    
+
+
+#     output_file="${output_dir}/${max_parallel_sessions}_session_output.txt"
+#     time_file="${time_dir}/${max_parallel_sessions}_session_time.txt"
+
+#     {
+#         time timeout --signal=INT "$timeout_duration" python3 ./main.py "$yearRodef" "$max_parallel_sessions" > "$output_file"
+#     } 2> "$time_file"
+# done
+
+# echo "Execution times and outputs for all specified years recorded in respective files."
